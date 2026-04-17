80500d6a406dd2d2adb29afb1872ee350db957bc
Shahzaman3 <faisalshahzaman8@gmail.com>
2026-04-17 16:06:52 +0530

    homepage added

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 // Pages
 import Hero from "./Pages/hero";
+import Homepage from "./Pages/Hompage";
 
 // Optional: global styles
 import "./App.css";
@@ -10,8 +11,10 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* Home Page */}
+        {/* Hero Landing Page */}
         <Route path="/" element={<Hero />} />
+        {/* Homepage */}
+        <Route path="/home" element={<Homepage />} />
       </Routes>
     </Router>
   );
